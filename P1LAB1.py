aaaa3159f19abf32f14a347d4a1174decb501917
jamD33 <jamesdesarae@gmail.com>
2026-09-17 06:30:38 -0400

    first python assignment
    taking input from user and printing output

---
+++ P1LAB1.py
@@ -0,0 +1,9 @@
+#Desarae James
+#9/13/2026
+#p1lab1.py
+#input and print 
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name + "!" "," "Welcome to CTI-110!")
