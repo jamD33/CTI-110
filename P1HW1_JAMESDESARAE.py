9cd084402269736b1ccb4be483be1006c59adc3a
jamD33 <jamesdesarae@gmail.com>
2026-09-19 09:54:13 -0400

    EXPONENTS, ADDITION, SUBTRACTION
    USING PYTHON

---
+++ P1HW1_JAMESDESARAE.py
@@ -0,0 +1,31 @@
+# Desarae James
+# P1HW1
+#Calculating exponents and addition and subtraction 
+
+# calculate exponents
+
+print("-------Exponents-------")
+print()
+
+base = int(input("Enter the base number: "))
+exponent = int(input("Enter the exponent: "))
+result = base ** exponent
+print(f"{base} raised to the power of {exponent} is {result}")
+
+# calculate addition
+print("-------Addition-------")
+print()
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+sum = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum}")
+
+# calculate subtraction
+print("-------Subtraction-------")
+print()
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+difference = num1 - num2
+print(f"The difference of {num1} and {num2} is {difference}")
